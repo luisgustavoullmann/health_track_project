25dedc8dcf1059936706afde9f8d639c6711d12c
Luis Gustavo Ullmann <luisgustavoullmann@gmail.com>
2018-09-17 22:16:26 -0300

    Version 2.9 - DB done

--- DB-Healthtrack/DB_HealthTrack.sql
+++ DB-Healthtrack/DB_HealthTrack.sql
@@ -7,10 +7,10 @@ CREATE TABLE t_dieta (
     dt_data             DATE NOT NULL
 );
 
-ALTER TABLE t_dieta ADD CONSTRAINT pk_ht_dieta PRIMARY KEY ( cd_dieta );
+ALTER TABLE t_dieta ADD CONSTRAINT t_dieta_pk PRIMARY KEY ( cd_dieta );
 
-CREATE TABLE t_end (
-    cd_end          NUMBER(6) NOT NULL,
+CREATE TABLE t_endereco (
+    cd_endereco     NUMBER(6) NOT NULL,
     ds_logradouro   VARCHAR2(20) NOT NULL,
     nm_logradouro   VARCHAR2(255) NOT NULL,
     nr_cep          VARCHAR2(10) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE t_end (
     nr_endereco     NUMBER(6) NOT NULL
 );
 
-ALTER TABLE t_end ADD CONSTRAINT pk_ht_end PRIMARY KEY ( cd_end );
+ALTER TABLE t_endereco ADD CONSTRAINT t_endereco_pk PRIMARY KEY ( cd_endereco );
 
 CREATE TABLE t_exercicio (
     cd_exercicio     NUMBER(6) NOT NULL,
@@ -32,7 +32,15 @@ CREATE TABLE t_exercicio (
     qt_padraotempo   TIMESTAMP(9) WITH LOCAL TIME ZONE NOT NULL
 );
 
-ALTER TABLE t_exercicio ADD CONSTRAINT pk_ht_exer PRIMARY KEY ( cd_exercicio );
+ALTER TABLE t_exercicio ADD CONSTRAINT t_exercicio_pk PRIMARY KEY ( cd_exercicio );
+
+CREATE TABLE t_peso (
+    cd_peso   NUMBER(6) NOT NULL,
+    nr_peso   NUMBER(5,2) NOT NULL,
+    dt_data   DATE NOT NULL
+);
+
+ALTER TABLE t_peso ADD CONSTRAINT t_peso_pk PRIMARY KEY ( cd_peso );
 
 CREATE TABLE t_prearterial (
     cd_pres_art   NUMBER(6) NOT NULL,
@@ -41,14 +49,31 @@ CREATE TABLE t_prearterial (
     ds_padrao     NUMBER(4,2) NOT NULL
 );
 
-ALTER TABLE t_prearterial ADD CONSTRAINT pk_ht_preart PRIMARY KEY ( cd_pres_art );
+ALTER TABLE t_prearterial ADD CONSTRAINT t_prearterial_pk PRIMARY KEY ( cd_pres_art );
 
-CREATE TABLE t_user (
-    cd_user         NUMBER(6) NOT NULL,
+CREATE TABLE t_user_end (
+    t_usuario_cd_usuario     NUMBER(6)
+        CONSTRAINT nnc_t_usend_t_user_cd_user NOT NULL,
+    t_endereco_cd_endereco   NUMBER(6)
+        CONSTRAINT nnc_t_usend_t_end_cd_end NOT NULL
+);
+
+ALTER TABLE t_user_end ADD CONSTRAINT t_user_end_pk PRIMARY KEY ( t_usuario_cd_usuario,
+t_endereco_cd_endereco );
+
+CREATE TABLE t_userpeso (
+    t_peso_cd_peso         NUMBER(6) NOT NULL,
+    t_usuario_cd_usuario   NUMBER(6) NOT NULL
+);
+
+ALTER TABLE t_userpeso ADD CONSTRAINT pk_ht_uspes PRIMARY KEY ( t_peso_cd_peso,
+t_usuario_cd_usuario );
+
+CREATE TABLE t_usuario (
+    cd_usuario      NUMBER(6) NOT NULL,
     nm_usuario      VARCHAR2(255) NOT NULL,
     nm_email        VARCHAR2(255) NOT NULL,
     nr_idade        NUMBER(3) NOT NULL,
-    nr_peso         NUMBER(5,2) NOT NULL,
     nr_altura       NUMBER(3,2) NOT NULL,
     nr_telefone     VARCHAR2(20) NOT NULL,
     nr_cpf          NUMBER(30) NOT NULL,
@@ -58,81 +83,78 @@ CREATE TABLE t_user (
     nr_password     VARCHAR2(255) NOT NULL
 );
 
-ALTER TABLE t_user ADD CONSTRAINT pk_ht_user PRIMARY KEY ( cd_user );
+ALTER TABLE t_usuario ADD CONSTRAINT t_usuario_pk PRIMARY KEY ( cd_usuario );
 
-CREATE TABLE t_user_art (
-    t_user_cd_user              NUMBER(6) NOT NULL,
+CREATE TABLE t_usuario_arterial (
+    t_usuario_cd_usuario        NUMBER(6) NOT NULL,
     t_prearterial_cd_pres_art   NUMBER(6) NOT NULL
 );
 
-ALTER TABLE t_user_art ADD CONSTRAINT pk_ht_usar PRIMARY KEY ( t_prearterial_cd_pres_art,
-t_user_cd_user );
+ALTER TABLE t_usuario_arterial ADD CONSTRAINT pk_ht_usar PRIMARY KEY ( t_usuario_cd_usuario,
+t_prearterial_cd_pres_art );
 
-CREATE TABLE t_user_dieta (
-    t_user_cd_user     NUMBER(6) NOT NULL,
-    t_dieta_cd_dieta   NUMBER(6) NOT NULL
+CREATE TABLE t_usuario_dieta (
+    t_usuario_cd_usuario   NUMBER(6) NOT NULL,
+    t_dieta_cd_dieta       NUMBER(6) NOT NULL
 );
 
-ALTER TABLE t_user_dieta ADD CONSTRAINT pk_ht_usdi PRIMARY KEY ( t_user_cd_user,
+ALTER TABLE t_usuario_dieta ADD CONSTRAINT pk_ht_usdi PRIMARY KEY ( t_usuario_cd_usuario,
 t_dieta_cd_dieta );
 
-CREATE TABLE t_user_end (
-    t_user_cd_user   NUMBER(6)
-        CONSTRAINT nnc_t_usend_t_user_cd_user NOT NULL,
-    t_end_cd_end     NUMBER(6)
-        CONSTRAINT nnc_t_usend_t_end_cd_end NOT NULL
-);
-
-ALTER TABLE t_user_end ADD CONSTRAINT pk_ht_user_ed PRIMARY KEY ( t_user_cd_user,
-t_end_cd_end );
-
-CREATE TABLE t_user_exercicio (
-    t_user_cd_user             NUMBER(6) NOT NULL,
+CREATE TABLE t_usuario_exercicio (
+    t_usuario_cd_usuario       NUMBER(6) NOT NULL,
     t_exercicio_cd_exercicio   NUMBER(6) NOT NULL
 );
 
-ALTER TABLE t_user_exercicio ADD CONSTRAINT pk_ht_usex PRIMARY KEY ( t_exercicio_cd_exercicio,
-t_user_cd_user );
+ALTER TABLE t_usuario_exercicio ADD CONSTRAINT pk_ht_usex PRIMARY KEY ( t_exercicio_cd_exercicio,
+t_usuario_cd_usuario );
 
-ALTER TABLE t_user_dieta
-    ADD CONSTRAINT fk_t_udieta_di FOREIGN KEY ( t_dieta_cd_dieta )
-        REFERENCES t_dieta ( cd_dieta );
-
-ALTER TABLE t_user_dieta
-    ADD CONSTRAINT fk_t_udieta_us FOREIGN KEY ( t_user_cd_user )
-        REFERENCES t_user ( cd_user );
-
-ALTER TABLE t_user_end
-    ADD CONSTRAINT fk_t_ue_end FOREIGN KEY ( t_end_cd_end )
-        REFERENCES t_end ( cd_end );
-
-ALTER TABLE t_user_end
-    ADD CONSTRAINT fk_t_ue_user FOREIGN KEY ( t_user_cd_user )
-        REFERENCES t_user ( cd_user );
-
-ALTER TABLE t_user_exercicio
-    ADD CONSTRAINT fk_t_uex_exer FOREIGN KEY ( t_exercicio_cd_exercicio )
-        REFERENCES t_exercicio ( cd_exercicio );
-
-ALTER TABLE t_user_exercicio
-    ADD CONSTRAINT fk_t_uex_user FOREIGN KEY ( t_user_cd_user )
-        REFERENCES t_user ( cd_user );
-
-ALTER TABLE t_user_art
-    ADD CONSTRAINT fk_t_upre_pre FOREIGN KEY ( t_prearterial_cd_pres_art )
+ALTER TABLE t_usuario_arterial
+    ADD CONSTRAINT fk_usar_ar FOREIGN KEY ( t_prearterial_cd_pres_art )
         REFERENCES t_prearterial ( cd_pres_art );
 
-ALTER TABLE t_user_art
-    ADD CONSTRAINT fk_tupre_us FOREIGN KEY ( t_user_cd_user )
-        REFERENCES t_user ( cd_user );
+ALTER TABLE t_usuario_arterial
+    ADD CONSTRAINT fk_usar_us FOREIGN KEY ( t_usuario_cd_usuario )
+        REFERENCES t_usuario ( cd_usuario );
 
+ALTER TABLE t_usuario_dieta
+    ADD CONSTRAINT fk_usdi_di FOREIGN KEY ( t_dieta_cd_dieta )
+        REFERENCES t_dieta ( cd_dieta );
+
+ALTER TABLE t_usuario_dieta
+    ADD CONSTRAINT fk_usdi_us FOREIGN KEY ( t_usuario_cd_usuario )
+        REFERENCES t_usuario ( cd_usuario );
+
+ALTER TABLE t_user_end
+    ADD CONSTRAINT fk_usen_end FOREIGN KEY ( t_endereco_cd_endereco )
+        REFERENCES t_endereco ( cd_endereco );
+
+ALTER TABLE t_user_end
+    ADD CONSTRAINT fk_usen_us FOREIGN KEY ( t_usuario_cd_usuario )
+        REFERENCES t_usuario ( cd_usuario );
+
+ALTER TABLE t_usuario_exercicio
+    ADD CONSTRAINT fk_usex_ex FOREIGN KEY ( t_exercicio_cd_exercicio )
+        REFERENCES t_exercicio ( cd_exercicio );
+
+ALTER TABLE t_usuario_exercicio
+    ADD CONSTRAINT fk_usex_us FOREIGN KEY ( t_usuario_cd_usuario )
+        REFERENCES t_usuario ( cd_usuario );
+
+ALTER TABLE t_userpeso
+    ADD CONSTRAINT fk_uspes_ps FOREIGN KEY ( t_peso_cd_peso )
+        REFERENCES t_peso ( cd_peso );
+
+ALTER TABLE t_userpeso
+    ADD CONSTRAINT fk_uspes_us FOREIGN KEY ( t_usuario_cd_usuario )
+        REFERENCES t_usuario ( cd_usuario );
 
 --	Consultar os dados de um determinado usuário (filtrar a partir do seu código);
 SELECT * FROM t_user WHERE cd_user = ?;
 --	Consultar todos os dados de todos os registros de peso de um determinado usuário, ordenando-os dos registros mais recentes para os mais antigos (filtrar a partir do seu código);
-SELECT nr_peso FROM t_user WHERE cd_user = ? ORDER BY nr_peso ASC;
+SELECT * FROM t_peso WHERE cd_user = ? ORDER BY dt_data ASC;
 --	Consultar todos os dados de um único registro de peso de um determinado usuário (filtrar a partir do código do usuário e do código de peso);
--- No meu modelo os dados de peso estão registrados na mesma tabela que o a do usuário. Devo alterar o projeto para ter uma tabela somente de peso?
+SELECT nr_peso FROM t_peso WHERE cd_user = ? AND cd_peso = ? ORDER BY dt_data ASC;
 --	Consultar todos os dados de todos os registros de pressão arterial de um determinado usuário, ordenando-os dos registros mais recentes para os mais antigos (filtrar a partir do seu código);
 SELECT * FROM t_prearterial WHERE cd_user = ? ORDER BY dt_data ASC;
 --	Consultar todos os dados de um único registro de pressão arterial de um determinado usuário (filtrar a partir do código do usuário e do código de pressão);
@@ -149,6 +171,8 @@ SELECT * FROM t_dieta WHERE cd_user = ? AND cd_dieta = ? ORDER BY dt_data ASC;
 -- INNER JOIN COMO DUAS OU MAIS TABELAS e peso é um atributo de Usuário, não fiz como entity.
 SELECT * 
 FROM t_user U INNER JOIN
-t_prearterial P 
+t_prearterial P
 USING (cd_user)
-ORDER BY nr_peso ASC AND dt_data ASC;
+INNER JOIN t_peso PS
+USING (cd_peso)
+ORDER BY PS.nr_peso ASC AND PS.dt_data ASC;
